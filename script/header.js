1628876c6cebb0c96d871fc4b37ba92de5f3f424
Martin Vrána <martin280901@gmail.com>
2025-01-14 21:04:14 +0100

    design 0.1

--- script/header.js
+++ script/header.js
@@ -1,5 +1,8 @@
 const links =[
-    ["Projekty", "/pages/projekty.html"]
+    ["Projekty", "./pages/projekty.html"],
+    ["Test link", "./pages/projekty.html"],
+    ["Test link", "./pages/projekty.html"],
+    ["Test link", "./pages/projekty.html"],
 ]
 
 function LoadHeader(){
@@ -7,23 +10,40 @@ function LoadHeader(){
 
     //Add logo to header
     var iconDiv = document.createElement("div")
-    iconDiv.className = "header-logo"
-    navBarDiv.appendChild(iconDiv)
+    iconDiv.className = "nav-bar-logo"
+    navBarDiv.appendChild(iconDiv);
 
     //Add div for links
     var linkDiv = document.createElement("div")
-    linkDiv.className = "header-links"
-    navBarDiv.appendChild(linkDiv)
+    linkDiv.className = "nav-bar-links"
+    navBarDiv.appendChild(linkDiv);
 
     //Add every link in 'links'
+    for(var i = 0; i < links.length; i++){
+        var linkElement = document.createElement("a")
+        linkElement.href = links[i][1]
+        linkElement.innerHTML = links[i][0]
+        linkElement.style = "color: var({0})".replace("{0}", i%2!=0?"--fototria-orange":"--fototria-purple")
+        linkDiv.appendChild(linkElement)
+    }
+    /* Stará verze, nemá index pro střídání barev
     links.forEach(link => {
         var linkElement = document.createElement("a")
         linkElement.href = link[1]
         linkElement.innerHTML = link[0]
         linkDiv.appendChild(linkElement)
     });
+    */
 }
 
-window.onload = (event) =>{
-    LoadHeader()
+function TransformNavBar(){
+    console.log(document.body.scrollTop)
+    var navBarDiv = document.getElementById("nav-bar")
+    var titleDiv = document.getElementById("title-background")
+
+    if (window.scrollY == 0){
+        navBarDiv.style = "animation: hideNavBar 1s; animation-fill-mode: forwards;"
+    }else{
+        navBarDiv.style = "animation: showNavBar 1s; animation-fill-mode: forwards;"
+    }
 }
--- script/script.js
+++ script/script.js
@@ -1,22 +1,17 @@
-//Animovaná hlavička
-window.addEventListener('scroll', function(event) {
-    const navBar = this.document.getElementById("nav-bar")
-    if (window.scrollY > 1){
-        navBar.classList.remove("headerBig")
-        navBar.classList.add("headerSmall")
-    }else{
-        navBar.classList.remove("headerSmall")
-        navBar.classList.add("headerBig")
-    }
-});
-
 function LoadPageIcon(){
+    console.log("test")
     link = document.createElement("link")
-    link.href = "../icon/favicon.png"
+    link.href = "./icon/favicon.png"
     link.rel = "icon"
     document.getElementsByTagName('head')[0].appendChild(link)
 }
+ 
+window.onscroll = () =>{
+    TransformNavBar()
+}
 
 window.onload = (event) =>{
-    
+    LoadPageIcon()
+    TransformNavBar()
+    LoadHeader()
 }
